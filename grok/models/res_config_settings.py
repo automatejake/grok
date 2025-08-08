810ed360dde99be05045e1db347e93a3ef43d809
Jacob Neubaum <jacob@neubaum.com>
2025-08-08 16:58:45 -0400

    change

--- grok/models/res_config_settings.py
+++ grok/models/res_config_settings.py
@@ -1,18 +1,18 @@
 from odoo import fields,models
 
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    grok_api_key = fields.Char()
+    grok_version = fields.Selection([('grok-3','Grok 3'),('grok-4','Grok 4')])
+
 class ResConfigSettings(models.TransientModel):
     _inherit = ['res.config.settings']
     
     grok_api_key = fields.Char(related='company_id.grok_api_key',
                                readonly=False,
                                )
-    grok_version = fields.Selection([('grok-3','Grok 3'),('grok-4','Grok-4')], 
+    grok_version = fields.Selection([('grok-3','Grok 3'),('grok-4','Grok 4')], 
                                     related='company_id.grok_version',
                                     readonly=False)
     
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    grok_api_key = fields.Char()
-    grok_version = fields.Selection([('grok-3','Grok 3'),('grok-4','Grok-4')])
